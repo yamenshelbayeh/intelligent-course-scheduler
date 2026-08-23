7291d781452ca275281ee68adb6b581a369a9a04
Yamen Shelbaya <yamenshelbayeh@gmail.com>
2026-08-24 01:45:27 +0300

    Complete database foundation and prerequisite graph

--- src/db.py
+++ src/db.py
@@ -22,8 +22,42 @@ def get_connection():
 
 def main():
     with get_connection() as connection:
-        print("Connected to PostgreSQL successfully!")
 
+        with connection.cursor() as cursor:
+
+            # Get every course
+            cursor.execute("""
+                SELECT course_code
+                FROM courses
+                ORDER BY course_code;
+            """)
+
+            courses = cursor.fetchall()
+
+            graph = {}
+
+            for course in courses:
+                graph[course[0]] = []
+
+            cursor.execute("""
+                SELECT
+                    c.course_code,
+                    p_course.course_code
+                FROM prerequisites p
+                JOIN courses c
+                    ON c.course_id = p.course_id
+                JOIN courses p_course
+                    ON p_course.course_id = p.prerequisite_id
+                ORDER BY c.course_code;
+            """)
+
+            prerequisites = cursor.fetchall()
+
+            # Add prerequisites to graph
+            for course, prerequisite in prerequisites:
+                graph[course].append(prerequisite)
+
+            print(graph)
 
 if __name__ == "__main__":
     main()
